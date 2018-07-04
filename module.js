b7b8c1ae1874be61fdae874b25f0e94af0b3e5ee
Alexander Kharkov <kharkovalexander@gmail.com>
2018-07-04 10:18:46 +0000

    - fix datasource plugin UI, created datasources now usable

--- module.js
+++ module.js
@@ -5,7 +5,19 @@ define([
 function(MonascaDatasource, MonascaQueryCtrl) {
   'use strict';
 
-  var MonascaConfigCtrl = function() {};
+  var MonascaConfigCtrl = function($scope) {
+    $scope.authModeChanged = function() {
+      $scope.ctrl.current.jsonData.keystoneAuth = ($scope.ctrl.current.jsonData.authMode == 'Keystone');
+    };
+
+    this.current.jsonData = this.current.jsonData || {authMode: 'Keystone', keystoneAuth: true};
+    Object.assign(this.current.jsonData, {authMode: 'Keystone', keystoneAuth: true}, this.current.jsonData);
+    if (!this.current.jsonData.keystoneAuth) {
+        this.current.jsonData.keystoneAuth = (this.current.jsonData.authMode == 'Keystone') || false;
+    } else {
+        this.current.jsonData.authMode = 'Keystone';
+    }
+  };
   MonascaConfigCtrl.templateUrl = "partials/config.html";
 
   var MonascaQueryOptionsCtrl = function() {};
